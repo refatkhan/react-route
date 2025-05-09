0eb5883cabe65c41a20e1ddb66ddf8f3bbea42c9
refatkhan <refatkhan897@gmail.com>
2025-05-10 00:49:36 +0600

    erroElement add and how to works

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -5,20 +5,22 @@ import Favourite from "../Favourite";
 export const router = createBrowserRouter([
     //default route jokhon kew visit korbe eita show korbe
     {
-
+        index: true,
+        Component: MainLayout,
+        errorElement: <p>There is no element in this route</p>
     },
-  {
-    path: "/",
-    Component: MainLayout,
-    children:[
-      {
-        path: "/favourite",
-        Component: Favourite,
-      }
-    ]
-  },
-  {
-    path: "/about",
-    element: <p>This is about </p>
-  }
+    {
+        path: "/",
+        Component: MainLayout,
+        children: [
+            {
+                path: "/favourite",
+                Component: Favourite,
+            }
+        ]
+    },
+    {
+        path: "/about",
+        element: <p>This is about </p>
+    }
 ]);
